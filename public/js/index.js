8929e0c57bf02d1dcb38c748afefb6dababd6811
Vinay Vyas <vbvyas@gmail.com>
2014-06-06 19:34:32 -0700

    Breaking down poll questions

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
   $('.selector').change(function() {
     var selection = $(this).val();
     if (selection == 0) return;
+    if (selection == 1) {
+      addSingleAnswerPoll();
+    }
+    if (selection == 2) {
+      addMultipleAnswerPoll();
+    }
+    if (selection == 3) {
+      addTextPoll();
+    }
     $('.questions').append("<div><input type='text' placeholder='Enter the question'/></div>");
   });
 });
